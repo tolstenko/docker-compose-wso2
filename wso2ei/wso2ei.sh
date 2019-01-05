708b13eeb62602916eadac6172de92368344a49e
Miguel Diaz <miguel.diaz@balsain.es>
2019-01-05 21:50:39 +0100

    Upgrade to docker-compose #7

--- wso2ei/wso2ei.sh
+++
@@ -1,8 +0,0 @@
-docker run --name=wso2ei \
--p 4100:4100 \
--p 8243:8243 \
--p 8280:8280 \
--p 9443:9443 \
---mount type=bind,source="$(pwd)"/conf,target=/home/wso2carbon/wso2ei-6.4.0/conf \
--v "$(pwd)"/logs:/home/wso2carbon/wso2ei-6.4.0/repository/logs \
--d wso2/wso2ei-integrator:6.4.0
--- wso2is/wso2is.sh
+++
@@ -1,9 +0,0 @@
-docker run --name=wso2is \
--p 4000:4000 \
--p 8000:8000 \
--p 9763:9763 \
--p 9443:9443 \
--p 10500:10500 \
---mount type=bind,source="$(pwd)"/conf,target=/home/wso2carbon/wso2is-5.7.0/repository/conf \
--v "$(pwd)"/logs:/home/wso2carbon/wso2is-5.7.0/repository/logs \
--d wso2/wso2is:5.7.0
